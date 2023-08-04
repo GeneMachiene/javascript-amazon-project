fb3941967243c42c7a683f594e8923ec2fab2287
Gene Anthony <88444596+GeneMachiene@users.noreply.github.com>
2023-08-04 13:43:23 +0800

    added remove function

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 1
@@ -28,4 +28,8 @@ export function addToCart(productID){
       quantity:1
     });
   }
+}
+
+export function removeFromCart(id) {
+  cart = cart.filter(i => id !== i.productID);
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import {formatCurrency} from "./utils/money.js"
+import { formatCurrency } from "./utils/money.js"
 
 let productHTML = '';
 
@@ -38,7 +38,8 @@ cart.forEach(item => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link"
+              data-product-id="${id}">
               Delete
             </span>
           </div>
@@ -93,3 +94,12 @@ cart.forEach(item => {
   `;
 });
 document.querySelector('.js-order-summary').innerHTML = productHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach(link => {
+    link.addEventListener('click', () =>{
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  });
